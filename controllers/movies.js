166f68eec341df343d78a4a10b4c117bf3ae94cc
Vladimir Stenin <voxelll@hotmail.com>
2023-07-21 02:07:54 +0500

    деплой сервера

---
+++ controllers/movies.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const Movie = require('../models/movie');
+const BadRequestError = require('../errors/bad-request-error');
+const ForbiddenError = require('../errors/forbidden-error');
+const NotFoundError = require('../errors/not-found-error');
+
+const { STATUS_CREATED } = require('../utils/constants');
+
+const { ValidationError, CastError } = mongoose.Error;
+
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .populate('owner')
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+  const owner = req.user._id;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    owner,
+    movieId,
+    nameRU,
+    nameEN,
+  })
+    .then((movie) => Movie.findById(movie._id)
+      .populate('owner')
+      .then((moviePopulated) => res.status(STATUS_CREATED).send(moviePopulated)))
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        next(new BadRequestError('Переданы некорректные данные в метод создания карточки.'));
+        return;
+      }
+      next(err);
+    });
+};
+
+const deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .orFail(() => {
+      throw new NotFoundError('Карточка не найдена.');
+    })
+    .then((movie) => {
+      if (req.user._id === movie.owner.toString()) {
+        Movie.deleteOne(movie)
+          .then(() => {
+            res.send(movie);
+          })
+          .catch(next);
+      } else {
+        next(new ForbiddenError('У вас нет прав удалять чужие фильмы.'));
+      }
+    })
+    .catch((err) => {
+      if (err instanceof CastError) {
+        next(new BadRequestError('Переданы некорректные данные в метод удаления фильма.'));
+        return;
+      }
+      next(err);
+    });
+};
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovie,
+};
